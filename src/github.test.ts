ece0186f9e2d602e13890b4e61ff7be40bcca1cd
Bryan Chen <xlchen1291@gmail.com>
2025-02-11 17:24:19 +1300

    github test requires github token

--- src/github.test.ts
+++ src/github.test.ts
@@ -1,19 +1,21 @@
-import { expect, test } from 'bun:test'
+import { describe, expect, test } from 'bun:test'
 import { Octokit } from '@octokit/core'
 import { fetchIssue, fetchPR } from './github.ts'
 
-test('fetchIssue', async () => {
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
+describe.skipIf(!process.env.GITHUB_TOKEN)('github', () => {
+  test('fetchIssue', async () => {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    })
+    const issue = await fetchIssue('polka-codes', 'action', 1, octokit)
+    expect(issue).toMatchSnapshot()
   })
-  const issue = await fetchIssue('polka-codes', 'action', 1, octokit)
-  expect(issue).toMatchSnapshot()
-})
 
-test('fetchPR', async () => {
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
+  test('fetchPR', async () => {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    })
+    const pr = await fetchPR('polka-codes', 'polka-codes', 95, octokit)
+    expect(pr).toMatchSnapshot()
   })
-  const pr = await fetchPR('polka-codes', 'polka-codes', 95, octokit)
-  expect(pr).toMatchSnapshot()
 })
